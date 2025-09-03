57af4cf48802ffaf655fd68564b150501a4fee6c
cezaralfredo <34128004+cezaralfredo@users.noreply.github.com>
2025-09-03 16:32:48 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -11,6 +11,9 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+
+
 );
 
 reportWebVitals();
